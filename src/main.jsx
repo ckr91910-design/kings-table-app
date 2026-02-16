d3f75519d589fe8e4cc86c64b97e3d16fbf004a5
ckr91910-design <ckr91910@gmail.com>
2026-02-17 08:57:47 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx' // 파일 이름의 대소문자가 GitHub과 일치해야 합니다.
+import App from './App' 
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+4.  **CSS 파일**: `src/index.css` 파일이 없으면 에러가 날 수 있으니, 빈 파일이라도 만들어 두세요.
