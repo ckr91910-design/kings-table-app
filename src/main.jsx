5fca32899f06f2b2733ed360ebef9f97475d4c14
ckr91910-design <ckr91910@gmail.com>
2026-02-17 08:37:11 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // 파일 이름의 대소문자가 GitHub과 일치해야 합니다.
+import App from './app.jsx' // 파일 이름의 대소문자가 GitHub과 일치해야 합니다.
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
